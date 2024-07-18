8ab1ce9ca08559586b89c9617b7f411ecb76f196
Anderson Luis Silva <andersonluis87@gmail.com>
2024-07-18 13:50:01 +0100

    feat: adding all permissions for this app

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,11 +1,25 @@
-import { defineAbilityFor } from '@sass/auth'
+import {
+  defineAbilityFor,
+  organizationSchema,
+  projectSchema,
+  userSchema,
+} from '@sass/auth'
 
-const ability = defineAbilityFor({ role: 'MEMBER' })
+const ability = defineAbilityFor({ role: 'MEMBER', id: 'user-1' })
+const adminAbility = defineAbilityFor({ role: 'ADMIN', id: 'user-2' })
 
-const userCanInviteSomeoneElse = ability.can('invite', 'User')
-const userCanDeleteOtherUsers = ability.can('delete', 'User')
-const userCannotDeleteOtherUsers = ability.cannot('delete', 'User')
+const project = projectSchema.parse({ id: 'project-1', ownerId: 'user-1' })
+const project2 = projectSchema.parse({ id: 'project-2', ownerId: 'user-2' })
 
-console.log('🚀 ~ userCanInviteSomeoneElse:', userCanInviteSomeoneElse)
-console.log('🚀 ~ userCanDeleteOtherUsers:', userCanDeleteOtherUsers)
-console.log('🚀 ~ userCannotDeleteOtherUsers:', userCannotDeleteOtherUsers)
+console.log(
+  'Can MEMBER update their own project?',
+  ability.can('update', project)
+) // true
+console.log(
+  'Can MEMBER update others project?',
+  ability.can('update', project2)
+) // false
+console.log(
+  'Can ADMIN update others project?',
+  adminAbility.can('update', project)
+) // true
