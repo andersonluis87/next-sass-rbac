ea85344af03525c22c5d9dc2d6286f655cca67a5
Anderson Luis Silva <andersonluis87@gmail.com>
2024-08-22 12:30:16 +0100

    feat: adding simple route for creating users

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -1,6 +1,9 @@
+import { hash } from 'bcryptjs'
 import type { FastifyInstance } from 'fastify'
 import type { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { z } from 'zod'
+
+import { prisma } from '@/lib/prisma'
 
 export async function createAccount(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
@@ -14,8 +17,31 @@ export async function createAccount(app: FastifyInstance) {
         }),
       },
     },
-    () => {
-      return 'User created'
+    async (request, response) => {
+      const { email, name, password } = request.body
+
+      const userAlredyExists = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      })
+
+      if (userAlredyExists) {
+        return response.status(400).send({
+          message: 'User already exists',
+        })
+      }
+
+      const passwordHash = await hash(password, 6)
+      await prisma.user.create({
+        data: {
+          email,
+          name,
+          passwordHash,
+        },
+      })
+
+      return response.status(201).send()
     }
   )
 }
