ff6164ef2189ff3cac80228e29aae0840cc0de78
Anderson Luis Silva <andersonluis87@gmail.com>
2025-02-18 12:41:53 +0000

    chore: lint

--- packages/auth/src/models/user.ts
+++ packages/auth/src/models/user.ts
@@ -1,7 +1,7 @@
-import { z } from 'zod'
+import { z } from "zod";
 
-import { rolesSchema } from '../roles'
+import { rolesSchema } from "../roles";
 
-export const userSchema = z.object({ id: z.string(), role: rolesSchema })
+export const userSchema = z.object({ id: z.string(), role: rolesSchema });
 
-export type User = z.infer<typeof userSchema>
+export type User = z.infer<typeof userSchema>;
